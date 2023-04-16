9e6827efe1669b7f8ec721b9eb5550c136752a86
Oniqued <zktl3575@gmail.com>
2023-04-16 13:19:16 +0900

    week5 exam commit

---
+++ week5/week5_exam.sql
@@ -0,0 +1,60 @@
+--5주차 실습 문제
+--
+--다음과 같은 기능을 하는 Stored Procedure들을 PL/SQL 프로그램을 이용하여 만드시오.
+--(프로시저 이름은 임의로 설정하며 바탕 테이블들은 수업 시간에 만든 stu, subject, sugang 테이블입니다.)
+--
+--1. stu 테이블에 학번, 이름, 학과를 입력받아 새로운 학생을 추가시키는 Stored Procedure를 만드시오.
+create or replace procedure exam01(
+    v_id in stu.stu_id%type,
+    v_name in stu.stu_name%type,
+    v_dept in stu.stu_dept%type)
+is
+begin 
+    insert into stu values (v_id, v_name, v_dept);
+end exam01;
+/
+
+exec exam01('0012345', 'kim', 'computer');
+select * from stu;
+
+--2. subject 테이블에 과목번호, 과목이름, 개설학과를 입력받아 새로운 과목을 추가시키는 Stored Procedure를 만드시오.
+create or replace procedure exam02(
+    v_sjid in subject.sj_id%type,
+    v_sjname in subject.sj_name%type,
+    v_sjdept in subject.sj_dept%type)
+is
+begin 
+    insert into subject values (v_sjid, v_sjname, v_sjdept);
+end exam02;
+/
+
+exec exam02('cm003', 'algorithm', 'computer');
+select * from subject;
+
+--3. 특정 학번을 입력받아 sugang 테이블에서 그 학생과 관련된 수강 내역을 삭제하고 stu 테이블에서도 삭제하는 Stored Procedure를 만드시오.
+create or replace procedure exam03(
+    v_stuid stu.stu_id%type)
+is
+begin
+    delete from sugang where stu_id = v_stuid;
+    delete from stu where stu_id = v_stuid;
+end exam03;    
+/
+
+exec exam03('0012345');
+select * from stu;
+
+--4. subject 테이블에서 과목번호를 입력받아 과목명을 출력하는 Stored Procedure를 만드시오.(프로시저를 실행하면 출력까지 다 되어야 합니다.)
+set serveroutput on
+create or replace procedure exam04(
+    v_sjid subject.sj_id%type)
+is
+    v_sjname subject.sj_name%type;
+begin 
+    select sj_name into v_sjname from subject where sj_id = v_sjid;
+    dbms_output.put_line('과목명: ' ||v_sjname);
+end exam04;    
+/
+
+exec exam04('cm001');
+--※ 값들을 입력받을 때는 C언어의 scanf처럼 키보드 타이핑으로 입력받는 게 아니라, execute exam1('9955555', 'gyebak', 'computer');이런 식으로 프로시저를 실행시킬 때 인자로 입력하면 됩니다.
---
+++ week5/week5_proc.sql
@@ -0,0 +1,172 @@
+Create or replace procedure test1
+(a IN number,
+ b IN OUT number,
+ c OUT number)
+IS
+Begin
+	c := 1234;
+End;
+/
+
+Create or replace procedure test2
+(sjid IN varchar2,
+ sjname IN varchar2,
+ sjdept IN varchar2)
+IS
+Begin
+	Insert into subject values 
+	(sjid, sjname, sjdept);
+End;
+/
+
+Create or replace procedure test3
+IS
+	v_stuid varchar2(7);
+	v_stuname stu.stu_name%TYPE;
+Begin
+	select stu_id, stu_name into v_stuid, v_stuname from stu
+	where stu_id = '9911111';
+	DBMS_OUTPUT.PUT_LINE('학번 : ' || v_stuid || ', 이름 : ' || v_stuname); 
+End;
+/
+
+Create or replace procedure test4
+( v_stuid IN sugang.stu_id%type,
+  v_sjid IN sugang.sj_id%type
+)
+IS
+	v_sugang sugang%ROWTYPE;
+Begin
+	v_sugang.stu_id := v_stuid;
+	v_sugang.sj_id := v_sjid;
+	Insert into sugang values (v_sugang.stu_id, v_sugang.sj_id); 
+End;
+/
+
+Create or replace procedure test5
+( v_stuid IN stu.stu_id%type )
+IS
+	v_stuname stu.stu_name%TYPE;
+	v_studept stu.stu_dept%TYPE;
+Begin
+	select stu_name, stu_dept into v_stuname, v_studept from stu
+	where stu_id = v_stuid;
+	if v_stuid = '9911111' then DBMS_OUTPUT.PUT_LINE('홍길동이네!!!');
+	else DBMS_OUTPUT.PUT_LINE('학번 : ' || v_stuid || ', 이름 : ' || v_stuname || ', 학과 : ' || v_studept);
+	end if;
+
+End;
+/
+
+Create or replace procedure test6
+(limit IN number )
+IS
+i number;
+Begin
+	for i in 1..5 loop
+		DBMS_OUTPUT.PUT_LINE(i);
+	end loop;
+
+	i := 0;	
+
+	while i < limit loop
+		DBMS_OUTPUT.PUT_LINE(i);
+ 		i := i + 1;		
+	end loop;
+End;
+/
+
+Create or Replace Function CHK_SAL
+(v_sal number)
+Return  number
+IS
+  v_chk number;
+Begin
+      v_chk :=  v_sal * 0.01;
+      return v_chk; 
+END;
+/
+
+-- Cursor 예제1. 학과가 'computer'인 학생들의 학번과 이름을 보여 주는 프로시저 만들기
+Create or replace procedure show_com
+IS
+   v_stuid  stu.stu_id%TYPE;
+   v_stuname  stu.stu_name%TYPE;
+   CURSOR stu_cursor IS
+   select stu_id, stu_name from stu
+   where stu_dept = 'computer';
+BEGIN
+   open stu_cursor;
+   loop 
+      fetch stu_cursor into v_stuid, v_stuname;
+      exit when stu_cursor%NOTFOUND;
+      
+      dbms_output.put_line(v_stuid || ' ' || v_stuname);
+   end loop;
+   close stu_cursor;
+END show_com;
+/
+
+Create or replace procedure show_com2
+IS
+   v_stu  stu%ROWTYPE;
+   CURSOR stu_cursor IS
+   select * from stu
+   where stu_dept = 'computer';
+BEGIN
+
+   for v_stu IN stu_cursor loop 
+
+      exit when stu_cursor%NOTFOUND;
+      dbms_output.put_line(v_stu.stu_id || ' ' || v_stu.stu_name);
+
+   end loop;
+   
+END show_com2;
+/
+
+-- Cursor 예제2. 커서 매개 변수로 학번을 집어넣어서 그 학번에 해당하는 학생이 수강하는 과목의
+--               과목 코드를 보여 주는 프로시저 만들기
+
+Create or replace procedure show_subid
+IS
+   v_sjid  sugang.sj_id%TYPE;
+
+   CURSOR sg_cursor(v_stuid varchar2) IS
+   select sj_id from sugang
+   where stu_id = v_stuid;
+BEGIN
+   open sg_cursor('9922222');
+   loop 
+      fetch sg_cursor into v_sjid;
+      exit when sg_cursor%NOTFOUND;
+      
+      dbms_output.put_line(v_sjid);
+   end loop;
+   close sg_cursor;
+END show_subid;
+/
+
+-- Cursor 예제3. stu 테이블에서 이름이 'sslee'인 학생의 이름을 'cmlee'로 바꾸는 프로시저 만들기
+
+Create or replace procedure up_name
+IS
+   v_stuname  stu.stu_name%TYPE;
+   CURSOR up_cursor IS
+   select stu_name from stu
+   where stu_name = 'sslee'
+   for update;
+BEGIN
+   open up_cursor;
+   loop 
+      
+      fetch up_cursor into v_stuname;
+      exit when up_cursor%NOTFOUND;
+      update stu set stu_name = 'cmlee'
+      where current of up_cursor; 
+      
+   end loop;
+   close up_cursor;
+END up_name;
+/
+
